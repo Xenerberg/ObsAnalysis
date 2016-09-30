09071eb738a79bf99fe6523a048a555ccbc6b542
Hrishik Mishra <hrishikmishra@yahoo.com>
2016-09-30 02:03:50 +0200

    Observability study

--- ObservabilityAnalyse.m
+++ ObservabilityAnalyse.m
@@ -1,5 +1,5 @@
 clc;clear all;
-syms q_0 q_1 q_2 q_3 om_x om_y om_z ita_1 ita_2 ita_3 ita_0 p_x p_y p_z;
+syms q_0 q_1 q_2 q_3 om_x om_y om_z ita_1 ita_2 ita_3 ita_0 p_x p_y p_z del_q_1 del_q_2 del_q_3 del_q_0 del_ita_1 del_ita_2 del_ita_3 del_ita_0;
 q_v = [q_1;q_2;q_3];
 q = [q_v;q_0];
 ita_v = [ita_1;ita_2;ita_3];
@@ -7,8 +7,11 @@ ita = [ita_v;ita_0];
 omega = [om_x;om_y;om_z];
 %p = [0.2;0.6;0.1];
 p = [p_x;p_y;p_z];
-x = [q;omega;p;ita];
-%x = [q;omega;ita];
+del_q = [del_q_1;del_q_2;del_q_3];
+del_ita = [del_ita_1;del_ita_2;del_ita_3];
+
+x = [del_q;omega;p;del_ita];
+%x = [del_q;omega;del_ita];
 %x = [q;omega;p];
 
 %R = (2*q_0^2 - 1)*eye(3,3) + 2*q_0*fn_VectorToSkewSymmetricTensor(q_v) + 2*q_v*q_v.';
@@ -18,10 +21,13 @@ q_dot = 0.5*fn_CrossTensor([omega;0],0)*q;
 omega_dot = psi;
 ita_dot = zeros(4,1);
 p_dot = zeros(3,1);
-f = [q_dot;omega_dot;p_dot;ita_dot];
-%f = [q_dot;omega_dot;ita_dot];
+del_q_dot = 0.5*fn_VectorToSkewSymmetricTensor(omega)*del_q;
+del_ita_dot = zeros(3,1);
+f = [del_q_dot;omega_dot;p_dot;del_ita_dot];
+%f = [del_q_dot;omega_dot;del_ita_dot];
 %f = [q_dot;omega_dot;p_dot];
-h = fn_CrossTensor(ita,0)*q;
+h = fn_CrossTensor([del_ita;1],0)*[del_q;1];
+h = h(1:3);
 
 row_1 = jacobian(h,x);
 
